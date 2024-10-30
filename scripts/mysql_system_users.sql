5832eb8996d610f95ee353fe861e178f08658978
Marco Tusa <tusamarco@gmail.com>
2024-10-30 17:51:26 +0100

    fix permission adding DELETE

--- scripts/mysql_system_users.sql
+++ scripts/mysql_system_users.sql
@@ -1,16 +1,15 @@
--- Copyright (c) 2018, 2024, Oracle and/or its affiliates.
+-- Copyright (c) 2018, 2023, Oracle and/or its affiliates.
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License, version 2.0,
 -- as published by the Free Software Foundation.
 --
--- This program is designed to work with certain software (including
+-- This program is also distributed with certain software (including
 -- but not limited to OpenSSL) that is licensed under separate terms,
 -- as designated in a particular file or component or in included license
 -- documentation.  The authors of MySQL hereby grant you an additional
 -- permission to link the program and your derivative works with the
--- separately licensed software that they have either included with
--- the program or referenced in the documentation.
+-- separately licensed software that they have included with MySQL.
 --
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -92,9 +91,8 @@ CREATE ROLE 'mysql.pxc.sst.role'@localhost;
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'mysql.pxc.sst.role'@localhost;
 
 GRANT BACKUP_ADMIN, LOCK TABLES, PROCESS, RELOAD, REPLICATION CLIENT, SUPER, CLONE_ADMIN,GROUP_REPLICATION_STREAM,SYSTEM_USER, SHUTDOWN,CONNECTION_ADMIN, CREATE USER ON *.* TO 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
-GRANT INSERT ON mysql.plugin TO 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
+GRANT INSERT,DELETE ON mysql.plugin TO 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
 GRANT CLONE_ADMIN, SYSTEM_VARIABLES_ADMIN, SUPER, SHUTDOWN, EXECUTE ON *.* to 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
-GRANT INSERT ON mysql.plugin to 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
 GRANT SELECT,UPDATE,INSERT ON performance_schema.* TO 'mysql.pxc.sst.role'@localhost WITH GRANT OPTION;
 
 
